b40038b708506e9daaf31d27b6303da0a5be7100
Antonino Calderone <antonino.calderone@gmail.com>
2020-04-19 15:05:21 +0100

    update license info

--- cpp/nu_terminal_input.cc
+++ cpp/nu_terminal_input.cc
@@ -2,20 +2,14 @@
 *  This file is part of nuTetris
 *
 *  nuTetris is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
+*  it under the terms of the MIT License.
 *
 *  nuTetris is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
+*  MIT License for more details.
 *
-*  You should have received a copy of the GNU General Public License
-*  along with nuTetris; if not, write to the Free Software
-*  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  US
-*
-*  Author: <antonino.calderone@ericsson.com>, <acaldmail@gmail.com>
+*  Author: <antonino.calderone@gmail.com>
 *
 */
 
